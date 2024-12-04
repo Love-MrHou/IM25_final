f4aad5bd8fe22c9f3d1a3edc2e3dabb50dc51c2a
WilliamCHIU-ETH <411035006@gms.ndhu.edu.tw>
2024-12-04 19:05:56 +0800

    update VR action.cs

--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -105,6 +105,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Boolean InterctUI_ainpc
+        {
+            get
+            {
+                return SteamVR_Actions.default_InterctUI_ainpc;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
